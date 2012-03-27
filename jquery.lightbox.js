9b2df260d6a61d368bc3cae585dfec094c618d74
ulfben <ulfben@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-27 22:08:04 +0000

    adding 'doLightbox'-javascript function to apply lightboxing at arbitrary times.
    git-svn-id: https://plugins.svn.wordpress.org/wp-jquery-lightbox/trunk@524491 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- jquery.lightbox.js
+++ jquery.lightbox.js
@@ -465,37 +465,39 @@
         jsonDataParser: null,
         followScroll: false,
         isIE8: false  //toyNN:internal value only
-    };
-	$(document).ready(function($){
-		var haveConf = (typeof JQLBSettings == 'object');
-		if(haveConf && JQLBSettings.resizeSpeed) {
-			JQLBSettings.resizeSpeed = parseInt(JQLBSettings.resizeSpeed);
-		}
-		if(haveConf && JQLBSettings.marginSize){
-			JQLBSettings.marginSize = parseInt(JQLBSettings.marginSize);
-		}
-		var default_strings = {
-			help: ' Browse images with your keyboard: Arrows or P(revious)/N(ext) and X/C/ESC for close.',
-			prevLinkTitle: 'previous image',
-			nextLinkTitle: 'next image',
-			prevLinkText:  '&laquo; Previous',
-			nextLinkText:  'Next &raquo;',
-			closeTitle: 'close image gallery',
-			image: 'Image ',
-			of: ' of ',
-			download: 'Download'
-		};
-		$('a[rel^="lightbox"]').lightbox({
-			adminBarHeight: $('#wpadminbar').height() || 0,
-			linkTarget: (haveConf && JQLBSettings.linkTarget.length) ? JQLBSettings.linkTarget : '_self',
-			displayHelp: (haveConf && JQLBSettings.help.length) ? true : false,
-			marginSize: (haveConf && JQLBSettings.marginSize) ? JQLBSettings.marginSize : 0,
-			fitToScreen: (haveConf && JQLBSettings.fitToScreen == '1') ? true : false,
-			resizeSpeed: (haveConf && JQLBSettings.resizeSpeed >= 0) ? JQLBSettings.resizeSpeed : 400,
-			displayDownloadLink: (haveConf && JQLBSettings.displayDownloadLink == '0') ? false : true,
-			navbarOnTop: (haveConf && JQLBSettings.navbarOnTop == '0') ? false : true,
-			//followScroll: (haveConf && JQLBSettings.followScroll == '0') ? false : true,
-			strings: (haveConf && typeof JQLBSettings.help == 'string') ? JQLBSettings : default_strings
-		});	
-	});	
+    };	
+	$(document).ready(doLightBox);	
 })(jQuery);
+//you can call this manually at any time to activate the lightboxing. (useful for ajax-loaded content)
+function doLightBox(){
+	var haveConf = (typeof JQLBSettings == 'object');
+	if(haveConf && JQLBSettings.resizeSpeed) {
+		JQLBSettings.resizeSpeed = parseInt(JQLBSettings.resizeSpeed);
+	}
+	if(haveConf && JQLBSettings.marginSize){
+		JQLBSettings.marginSize = parseInt(JQLBSettings.marginSize);
+	}
+	var default_strings = {
+		help: ' Browse images with your keyboard: Arrows or P(revious)/N(ext) and X/C/ESC for close.',
+		prevLinkTitle: 'previous image',
+		nextLinkTitle: 'next image',
+		prevLinkText:  '&laquo; Previous',
+		nextLinkText:  'Next &raquo;',
+		closeTitle: 'close image gallery',
+		image: 'Image ',
+		of: ' of ',
+		download: 'Download'
+	};
+	jQuery('a[rel^="lightbox"]').lightbox({
+		adminBarHeight: jQuery('#wpadminbar').height() || 0,
+		linkTarget: (haveConf && JQLBSettings.linkTarget.length) ? JQLBSettings.linkTarget : '_self',
+		displayHelp: (haveConf && JQLBSettings.help.length) ? true : false,
+		marginSize: (haveConf && JQLBSettings.marginSize) ? JQLBSettings.marginSize : 0,
+		fitToScreen: (haveConf && JQLBSettings.fitToScreen == '1') ? true : false,
+		resizeSpeed: (haveConf && JQLBSettings.resizeSpeed >= 0) ? JQLBSettings.resizeSpeed : 400,
+		displayDownloadLink: (haveConf && JQLBSettings.displayDownloadLink == '0') ? false : true,
+		navbarOnTop: (haveConf && JQLBSettings.navbarOnTop == '0') ? false : true,
+		//followScroll: (haveConf && JQLBSettings.followScroll == '0') ? false : true,
+		strings: (haveConf && typeof JQLBSettings.help == 'string') ? JQLBSettings : default_strings
+	});	
+}
